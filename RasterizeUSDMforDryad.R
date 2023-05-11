b7faeda49b43270c746132871658f8d4d3f771c7
heplersa <38919454+heplersa@users.noreply.github.com>
2023-05-11 13:20:07 -0400

    Add files via upload

---
+++ RasterizeUSDMforDryad.R
@@ -0,0 +1,178 @@
+## This is an R script to rasterize the USDM to a gridded support.
+##
+## 1. Obtain raw data
+#######################
+#######################
+
+
+## 2. Libraries
+#################################
+library(sf)
+library(raster) ## install version 3.5.29 from source.  
+library(ggplot2)
+library(RColorBrewer)
+library(maps)
+library(fields)
+#################################
+
+
+## 3. Code to load one USDM file, set things up, make adjacency matrix
+#################################
+usdm=st_read("USDM/USDM_20140729_M/USDM_20140729.shp") ## Chosen due to severity of drought
+head(usdm)
+
+usa <- getData("GADM",country="USA",level=1)
+orng=brewer.pal(6, "Oranges")
+
+## Rasterize this to just a regular rectangular grid
+rst_template <- raster(ncols = 120, nrows = 50,
+                       crs = projection(usdm),
+                       ext = extent(c(-125,-65,25,50)))
+raster_data <- rasterize(usdm, rst_template)
+plot(raster_data)
+
+## Quiltplot with state lines
+lon=seq(-124.75,-65.25,by=0.5)
+lat=seq(49.75,25.25,by=-0.5)
+quilt.plot(expand.grid(lon, lat),c(raster_data[]), nx=120, ny=50, xlab="Longitude", ylab="Latitude", main="USDM", col=orng)
+map("state",lwd=1,add=T)
+
+## Need to intersect raster with outer shape of US
+us.grid=rasterize(usa,rst_template)
+us.grid=(us.grid>0)-2 ## Make a 0-map for valid locations
+plot(us.grid) ## Should be -1 if in the contiguous US
+
+## Can restrict plot using mask function
+usdmr <- mask(crop(raster_data, us.grid), us.grid)
+plot(usdmr, col=orng)
+map("state",lwd=1,add=T)
+
+## Differentiate between NA (outside contiguous US) and level 0.
+dm=usdmr
+for (i in 1:50){
+  print(i)
+  for (j in 1:120) {
+    if(is.na(us.grid[i,j])=="FALSE") {
+      if(is.na(usdmr[i,j])=="TRUE") {dm[i,j]=0} else {}
+    }
+  }
+}
+
+## NA means outside US.  0 means no response.  D0 through D4 are coded 1 to 5.
+plot(dm, col=orng)
+
+## Need response vector (0,1,2, etc.) for valid cells
+dm[is.na(us.grid)==FALSE]
+
+## Adjancancy Matrix
+wh=which(is.na(us.grid[])=="FALSE") ## wh count relevant cells on the full extent
+coord=expand.grid(lon, lat)
+coord.use=coord[wh,]
+
+plot(coord.use, cex=0.2, pch=16)
+
+#us.grid.test=us.grid[]
+#us.grid.test[1]=1 ## bottom left
+#us.grid.test[120]=1 ## bottom right
+#us.grid.test[121]=1
+#try=matrix(us.grid.test[],nrow=120,ncol=50)
+#image(try) ## Yeah it's upside down, just seeing if this box contained the continental US
+
+name1=c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "AA","BB","CC","DD","EE","FF","GG","HH","II","JJ","KK","LL","MM","NN","OO","PP","QQ","RR","SS","TT","UU","VV","WW","XX")
+name2=seq(1,120,by=1)
+
+name3a=expand.grid(name2,name1)
+name3=data.frame(name3a[,2],name3a[,1])
+
+name4=character()
+for (i in 1:6000){
+  name4[i]=paste(name3[i,1],name3[i,2],sep="")
+}
+head(name4)
+labels=name4[is.na(us.grid[])=="FALSE"]
+
+dz=as.matrix(dist(coord.use))
+
+A = matrix(0,dim(dz)[1],dim(dz)[1])
+rownames(A)=wh
+colnames(A)=wh
+
+for (i in 1:dim(dz)[1]){
+  print(i)
+  tmp=which((dz[i,] > 0.01) & (dz[i,]<0.71))
+  A[i,tmp]=1
+}
+
+rownames(A)=labels
+colnames(A)=labels
+############################
+
+
+## 4. Read in all years of USDM shapefiles, rasterize, save .csv
+#############################
+yearseq = seq(2000,2022,by=1)
+filenames=character()
+setwd("C:/Users/erhardrj/Desktop/Everything/Research/Drought/Full Dataset/USDM")
+for (i in 1:length(yearseq)){
+  dirs = list.dirs(paste(yearseq[i],"_USDM_M", sep = ""))
+  for (j in 2:length(dirs)){
+    newfilenames <- Sys.glob(paste(dirs[j],"/*.shp",sep=""))
+    filenames=append(filenames, newfilenames)
+  }
+}
+
+filenames
+
+dm=list()
+drought=numeric()
+for (q in 1:length(filenames)){
+  usdm=st_read(filenames[q], quiet=TRUE)
+  cat(q)
+  ##   usdm=dplyr::select(usdm, "DM")
+  raster_data <- rasterize(usdm, rst_template)
+  usdmr <- mask(crop(raster_data, us.grid), us.grid)
+  
+  dm[[q]]=usdmr
+  dm[[q]][(is.na(us.grid[])=="FALSE") & (is.na(usdmr[])=="TRUE")]=0
+  for (i in 1:50){
+    print(q+i/100)
+    for (j in 1:120) {
+      if(is.na(us.grid[i,j])=="FALSE") {
+        if(is.na(usdmr[i,j])=="TRUE") {dm[[q]][i,j]=0} else {}
+      }
+    }
+  }
+  tmp=dm[[q]][]
+  tmp=tmp[is.na(tmp)=="FALSE"]
+  tmp2=tmp
+  for (m in 1:length(data.frame(usdm)[,1])){
+    tmp2[tmp2==data.frame(usdm)[m,1]]=paste("D",data.frame(usdm)[m,2],sep="")
+  }
+  drought=append(drought,tmp2)
+}
+drought
+
+time=character()
+for (i in 1:length(filenames)){
+  cat(i, sep="\n")
+  tmp=dm[[i]][]
+  tmp=tmp[is.na(tmp)=="FALSE"]
+  hold=rep(substring(filenames[i],18,25),length(tmp))
+  time=append(time,hold)
+}
+time
+
+name5=rep(labels,length(filenames))
+coord1=rep(coord.use[,1],length(filenames))
+coord2=rep(coord.use[,2],length(filenames))
+
+data=data.frame(time,name5,coord1,coord2,drought[1:length(time)])
+colnames(data)=c("time","grid","lon","lat","drought")
+table(data$drought)
+save.image(file="USDMRasterized.rda")
+
+write.csv(data, file="usdm.csv")
+##########################################
+
+
---
+++ TeleconnectionsForDryad.R
@@ -0,0 +1,115 @@
+## This script extracts raw teleconnections data from
+## https://www.cpc.ncep.noaa.gov
+## and formats it to weekly data to match the USDM drought data
+
+## 1. Load libraries
+library(stringr)
+
+## 2. ENSO
+enso <- read.fwf(
+  file=url("https://www.cpc.ncep.noaa.gov/data/indices/wksst9120.for"),
+  skip=4,
+  widths=c(12, 7, 4, 9, 4, 9, 4, 9, 4)
+)
+
+enso <- enso[,c(1,7)]
+head(enso)
+str_sub(enso[,1], start=-6)
+enso <- enso[-c(1:957),] ## Remove dates before Year 2000
+enso <- enso[1:1174,]
+head(enso)
+tail(enso)
+
+plot(enso[,2], type="l", xlab="Week")
+
+year = as.numeric(str_sub(enso[,1], start=-6, end=-3))
+day = as.numeric(str_sub(enso[,1], start=2, end=3))
+
+mon.txt = str_sub(enso[,1], start=4, end=6)
+mon = array()
+mon[mon.txt=="JAN"] = "01"
+mon[mon.txt=="FEB"] = "02"
+mon[mon.txt=="MAR"] = "03"
+mon[mon.txt=="APR"] = "04"
+mon[mon.txt=="MAY"] = "05"
+mon[mon.txt=="JUN"] = "06"
+mon[mon.txt=="JUL"] = "07"
+mon[mon.txt=="AUG"] = "08"
+mon[mon.txt=="SEP"] = "09"
+mon[mon.txt=="OCT"] = "10"
+mon[mon.txt=="NOV"] = "11"
+mon[mon.txt=="DEC"] = "12"
+
+enso.wk = data.frame(year, mon, day, enso[,2])
+head(enso.wk)
+tail(enso.wk)
+## Matches by one day (i.e. 1/5/2000 goes with 1/4/2000 in final dataset)
+
+## 3. PNA
+pna <- read.fwf(
+  file=url("https://ftp.cpc.ncep.noaa.gov/cwlinks/norm.daily.pna.index.b500101.current.ascii"),
+  skip=0,
+  widths=c(4, 3, 3, 7)
+)
+colnames(pna) <- c("year","month","day","pna")
+
+wh = which(pna$year == 2000 & pna$month == 1 & pna$day == 4)
+## upper=ceiling((dim(pna)[1] - wh)/7)
+upper = 1174
+
+pna.wk = pna[(wh+7*(seq(1,upper)-1)),]
+tail(pna.wk)
+
+pna.avg = rowMeans(matrix(as.numeric(pna$pna[(wh-6):26477]), ncol = 7, byrow=TRUE), na.rm=TRUE)
+tail(pna.avg)
+pna.wk[,4] <- pna.avg
+
+
+## 4. NAO
+nao <- read.fwf(
+  file=url("https://ftp.cpc.ncep.noaa.gov/cwlinks/norm.daily.nao.index.b500101.current.ascii"),
+  skip=0,
+  widths=c(4, 3, 3, 7)
+)
+colnames(nao) <- c("year","month","day","nao")
+
+wh = which(nao$year == 2000 & nao$month == 1 & nao$day == 4)
+nao.wk = nao[(wh+7*(seq(1,upper)-1)),]
+tail(nao.wk)
+
+nao.avg = rowMeans(matrix(as.numeric(nao$nao[(wh-6):26477]), ncol = 7, byrow=TRUE), na.rm=TRUE)
+tail(nao.avg)
+nao.wk[,4] <- nao.avg
+
+
+## 5. AO
+ao <- read.fwf(
+  file=url("https://ftp.cpc.ncep.noaa.gov/cwlinks/norm.daily.ao.index.b500101.current.ascii"),
+  skip=0,
+  widths=c(4, 3, 3, 7)
+)
+colnames(ao) <- c("year","month","day","ao")
+
+wh = which(ao$year == 2000 & ao$month == 1 & ao$day == 4)
+ao.wk = ao[(wh+7*(seq(1,upper)-1)),]
+tail(ao.wk)
+
+ao.avg = rowMeans(matrix(as.numeric(ao$ao[(wh-6):26477]), ncol = 7, byrow=TRUE), na.rm=TRUE)
+tail(ao.avg)
+ao.wk[,4] <- ao.avg
+
+## 6. Combine teleconnections into common format
+head(pna.wk)
+time = pna.wk$day*1 + pna.wk$month*100 + pna.wk$year*10000
+tele <- data.frame(time, pna.wk$pna, nao.wk$nao, ao.wk$ao)
+enso.wk[1:dim(tele)[1],4]
+tele <- cbind(tele, enso.wk[1:dim(tele)[1],4])
+colnames(tele) <- c("time","pna","nao","ao","enso")
+
+tele$pna <- round(tele$pna, 2)
+tele$nao <- round(tele$nao, 2)
+tele$ao <- round(tele$ao, 2)
+
+head(tele)
+write.csv(file="Teleconnections/tele2.csv", tele, row.names=FALSE)
+
